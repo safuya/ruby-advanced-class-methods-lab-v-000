9561f3e3dff96911f0753d3d4685be2743bc6c29
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-26 13:38:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,11 @@ class Song
 
   def self.find_or_create_by_name(name)
     if self.find_by_name(name)
-      self.find_by_name(name)
+      song = self.find_by_name(name)
     else
-      self.create_by_name(name)
+      song = self.create_by_name(name)
     end
+    song
   end
 
   def self.alphabetical
