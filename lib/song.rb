89e0ff3856b330a083f81546f9170acb22d1bfdf
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-26 13:19:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,12 @@ class Song
     self.all.detect { |song| song.name == name }
   end
 
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
 end
